52a9455823461fb07f1550df927e44cd2d1da7d0
Defy-h0dl <78126213+Defy-h0dl@users.noreply.github.com>
2021-05-17 21:10:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(https://nftchat-j26q2miyf-defy-h0dl.vercel.app, () => {
+  console.log('welcome');
 });
 io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
 io.on('connection', (socket) => {
